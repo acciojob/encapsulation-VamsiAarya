4f99f2cd5da3f841a92690a63382252669e1a920
Vamsi krishna <vamcaarya31@gmail.com>
2022-12-10 07:27:59 +0530

    Added getters and setters..implemented encapsulation

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,15 @@
 package com.driver;
 
 public class Main {
-  
+
+    public static void main(String[] args)
+    {
+        RWOnly obj1 = new RWOnly();
+
+        obj1.setName("Vamsi");
+
+        System.out.println(obj1.getName());
+
+    }
+
 }
